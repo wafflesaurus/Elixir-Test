b6fcad7d482c6f78e2eb1ce2c2fe4a5021a15a78
wafflesaurus <ericbrusven@gmail.com>
2017-02-20 23:24:18 -0600

    Finish test server

--- lib/database_server.ex
+++ lib/database_server.ex
@@ -3,6 +3,18 @@ defmodule DatabaseServer do
     spawn(&loop/0)
   end
 
+  def run_async(server_pid, query_def) do
+    send(server_pid, {:run_query, self, query_def})
+  end
+
+  def get_result do
+    receive do
+      {:query_result, result} -> result
+    after 5000 ->
+      {:error, :timeout}
+    end
+  end
+
   defp loop do
     receive do
       {:run_query, caller, query_def} ->
